46037158ac6766ca730528b5054f36938838f231
Ayush <ayush20lv@gmail.com>
2026-01-07 11:03:28 +0000

    datatypes discussed

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use scrict"; // treat all JS code as newer version //
+
+// alert( 3 + 3) // we are using node.js, not browser
+
+// discussing data types below : -
+
+let name = "AYUSH"
+let age = 25
+let isLoggedIn = true 
+
+// number => 2 to power 53
+// bigint
+// string => "AYUSH"
+// boolean => true/flase
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof "AYUSH");
+console.log(typeof null);
